b84387032beb41a8671043c63878cddb86e37c6d
Eduardo Torres <contato@eduardotorres.dev>
2021-11-02 11:57:03 -0300

    fix: description field

--- src/resources/views/player.blade.php
+++ src/resources/views/player.blade.php
@@ -1,32 +1,31 @@
-<audio-player
-    data-src="{{ $src }}"
-    data-title="{{ $title }}"
-    data-description="{{ $description }}">
+<audio-player data-src="{{ $src }}" data-title="{{ $title }}" data-description="{{ $description }}">
     <div class="audio">
         <audio src="{{ $src }}" preload="metadata"></audio>
         <h1 class="audio__title">{{ $title }}</h1>
         <div class="audio__folder">
             <span>{{ config('audio-player.title', 'Audio Player') }}</span>
         </div>
-            <button class="audio__button audio__play">
-                <span>Play/Stop</span>
-            </button>
-            <div class="audio__time time">
-                <span class="time__current">0:00</span>
-                <span>/</span>
-                <span class="time__duration">0:00</span>
-            </div>
+        <button class="audio__button audio__play">
+            <span>Play/Stop</span>
+        </button>
+        <div class="audio__time time">
+            <span class="time__current">0:00</span>
+            <span>/</span>
+            <span class="time__duration">0:00</span>
+        </div>
+        <div class="audio__timeline">
+            <input type="range" class="audio__slider slider" max="100" value="0">
+        </div>
+        <div class="audio__volume volume">
             <div class="audio__timeline">
-                <input type="range" class="audio__slider slider" max="100" value="0">            
+                <input type="range" class="volume__slider audio__slider" max="100" value="100">
             </div>
-            <div class="audio__volume volume">
-                <div class="audio__timeline">
-                    <input type="range" class="volume__slider audio__slider" max="100" value="100">
-                </div>
-                <button class="audio__button volume__button">
-                    <span>Volume</span>
-                </button>
-            </div>
-        <h1 class="audio__description">{{ $description }}</h1>
+            <button class="audio__button volume__button">
+                <span>Volume</span>
+            </button>
+        </div>
+        <span class="audio__description">
+            {!! html_entity_decode($description) !!}
+        </span>
     </div>
 </audio-player>
